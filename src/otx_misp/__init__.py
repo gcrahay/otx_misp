5359d511528a9df47ad861785fd11e2f2eb72751
Nick Driver <driver.nick@gmail.com>
2017-03-24 13:03:50 -0400

    Update __init__.py

--- src/otx_misp/__init__.py
+++ src/otx_misp/__init__.py
@@ -207,7 +207,7 @@ def create_events(pulse_or_list, author=False, server=False, key=False, misp=Fal
     if 'references' in pulse:
         for reference in pulse['references']:
             log.info("\t - Adding external analysis link: {}".format(reference))
-            if misp and reference:
+            if misp:
                 misp.add_named_attribute(event, 'link', reference, category='External analysis')
             result_event['attributes']['references'].append(reference)
 
