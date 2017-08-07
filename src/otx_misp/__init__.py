a2520c0df9af68c0c8a94f4c72568d77c74e7e2b
Olivier BERT <olivier.bert@cert-ist.com>
2017-08-07 17:09:37 +0200

    Use the pulse's creation date instead of the modified to create the MISP event

--- src/otx_misp/__init__.py
+++ src/otx_misp/__init__.py
@@ -209,7 +209,7 @@ def create_events(pulse_or_list, author=False, server=False, key=False, misp=Fal
     else:
         event_name = pulse['name']
     try:
-        dt = date_parser.parse(pulse['modified'])
+        dt = date_parser.parse(pulse['created'])
     except (ValueError, OverflowError):
         log.error("Cannot parse Pulse 'modified' date.")
         dt = datetime.utcnow()
